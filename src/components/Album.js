406bff234a6060343cad5e35afaa0de53a528002
Jill <globaldetroithuman@gmail.com>
2018-07-17 23:18:31 -0400

    HW8 Update Complete

--- src/components/Album.js
+++ src/components/Album.js
@@ -35,7 +35,7 @@ class Album extends Component {
 			return (<div className="ion-play"></div>);
 		} else if (this.state.hoveredIndex === index && this.state.currentSong === song && this.state.isHovering && this.state.isPlaying) {
 			return (<div className="ion-pause"></div>);
-		} else if (this.state.hoveredIndex === index && this.state.currentSong === song.pause && this.state.isHovering) {
+		} else if (this.state.hoveredIndex === index && this.state.currentSong !== song.pause && this.state.isHovering) {
 			return (<div className="ion-play"></div>);
 		}		
 		else {

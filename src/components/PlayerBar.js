2b2251025967c400d18ebea10a14602af00b6122
Jill <globaldetroithuman@gmail.com>
2018-07-30 04:13:19 -0400

    Update Master HW9 Playerbar

--- src/components/PlayerBar.js
+++ src/components/PlayerBar.js
@@ -3,7 +3,45 @@ import Slider from 'react-rangeslider';
 import 'react-rangeslider/lib/index.css';
 
 class PlayerBar extends Component {
+
+	formatTime(time) {
+		let timeSt = time.toString();
+		let minutes = Math.floor(timeSt/60);
+  		let seconds = timeSt - minutes * 60;
+  		if (seconds < 10 && minutes === 0) {
+  			timeSt = ':'+'0'+seconds;
+  			timeSt = timeSt.slice(0,3);
+  		}
+  		else if (timeSt < 60 && minutes === 0) {
+  			timeSt = ':'+seconds;
+  			timeSt = timeSt.slice(0,3);
+  		}
+  		else if (timeSt > 59 && seconds < 10) {
+  			timeSt = minutes+':'+'0'+seconds;
+  			timeSt = timeSt.slice(0,4);
+  		}
+  		else {
+  			timeSt = minutes+':'+seconds;
+  			timeSt = timeSt.slice(0,4);
+  		}
+  		return timeSt;
+  	}
+
+  	formatDuration(duration) {
+  		if (isNaN(duration)) {
+  			return "Select a song";
+  		} else {
+  		let durSt = duration.toString();
+  		let minutes = Math.floor(durSt/60);
+  		let seconds = durSt - minutes * 60;
+  		let minSec = minutes+':'+seconds;
+  		return minSec.slice(0,4);
+  	}
+  	}
+
 	render() {
+		let time = this.props.currentTime;
+		let duration = this.props.duration;
 		return (
 			<section className="player-bar">
 				<section id="buttons">
@@ -18,7 +56,7 @@ class PlayerBar extends Component {
 					</button>
 				</section>
 				<section id="time-control">
-					<div className="current-time">{this.props.currentTime}</div>
+					<div className="current-time">{this.formatTime(time)}</div>
 					<input 
 						type="range" 
 						className="seek-bar" 
@@ -28,7 +66,7 @@ class PlayerBar extends Component {
 						step="0.01"
 						onChange={this.props.handleTimeChange}
 						/>
-					<div className="total-time">{this.props.duration}</div>
+					<div className="total-time">{this.formatDuration(duration)}</div>
 				</section>
 				<section id="volume-control">
 				<div className="icon ion-volume-low"></div>
